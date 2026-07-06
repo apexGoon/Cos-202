d7bba0072f2c1d715f7188c31eefc43fba9c9252
apexGoon <apexlearning13@gmail.com>
2026-07-06 07:30:44 -0400

    Create MathematicalCalculator.py

---
+++ MathematicalCalculator.py
@@ -0,0 +1,25 @@
+print("=== MATHEMATICAL CALCULATOR ===")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /, %, ^): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Division by zero is not allowed."
+elif operator == "%":
+    result = num1 % num2
+elif operator == "^":
+    result = num1 ** num2
+else:
+    result = "Invalid operator."
+
+print("Result:", result)
